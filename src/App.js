cc0f8e5dd363266a62b51dc1f4f45be1ee1146cf
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2021-10-01 17:47:16 +0500

    frontend

--- src/App.js
+++ src/App.js
@@ -6,16 +6,25 @@ import contract from "./build/contracts/Bep20Token.json";
 class App extends React.Component {
   async componentWillMount() {
     this.loadWeb3();
+    this.loadBlockchainData();
+  }
+
+  componentDidMount() {
+    document.body.style.backgroundColor = "#282c34";
   }
 
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.sendToken = this.sendToken.bind(this);
+    this.loadBlockchainData = this.loadBlockchainData.bind(this);
     this.state = {
       receiver: "",
-      amount: 0,
+      amount: "",
       account: "",
+      totalSupply: "",
+      symbol: "",
+      balance: "",
     };
   }
 
@@ -61,29 +70,109 @@ class App extends React.Component {
       });
 
     const totalSupply = await instance.methods.totalSupply().call();
+    this.setState({ totalSupply: totalSupply });
     console.log("Total Supply" + totalSupply);
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const webeProvider = new Web3(Web3.givenProvider);
+    const accounts = await webeProvider.eth.getAccounts();
+
+    this.setState({ account: accounts[0] });
+    console.log("Sender :  " + accounts[0]);
+
+    const networkId = await web3.eth.net.getId();
+    const deployedNetwork = contract.networks[networkId];
+
+    const instance = new web3.eth.Contract(
+      contract.abi,
+      deployedNetwork.address
+    );
+
+    const totalSupply = await instance.methods.totalSupply().call();
+    const symbol = await instance.methods.symbol().call();
+    const balance = await instance.methods.balanceOf(accounts[0]).call();
+    this.setState({ totalSupply: totalSupply });
+    this.setState({ symbol: symbol });
+    this.setState({ balance: balance });
   }
 
   render() {
     return (
       <div className="App">
-        <input
-          type="text"
-          name="receiver"
-          placeholder="Receiver"
-          value={this.state.receiver}
-          onChange={this.handleChange}
-        />
+        <div>
+          <h1 style={{ color: "#f35f25" }}>Bep20 Token</h1>
 
-        <input
-          type="number"
-          name="amount"
-          placeholder="Amount"
-          value={this.state.amount}
-          onChange={this.handleChange}
-        />
+          <span
+            style={{
+              float: "left",
+              color: "#fff",
+              fontSize: "20px",
+              marginLeft: "10px",
+            }}
+          >
+            Total Supply: {this.state.totalSupply + " " + this.state.symbol}
+          </span>
 
-        <button onClick={this.sendToken}>Send</button>
+          <span
+            style={{
+              float: "right",
+              color: "#fff",
+              fontSize: "20px",
+              marginRight: "10px",
+            }}
+          >
+            Balance: {this.state.balance + " " + this.state.symbol}
+          </span>
+
+          <br />
+          <hr />
+
+          <input
+            style={{
+              marginTop: "30px",
+              width: "450px",
+              height: "25px",
+              fontSize: "18px",
+            }}
+            type="text"
+            name="receiver"
+            placeholder="Receiver Address"
+            value={this.state.receiver}
+            onChange={this.handleChange}
+          />
+
+          <div>
+            <input
+              style={{
+                marginTop: "10px",
+                width: "450px",
+                height: "25px",
+                fontSize: "18px",
+              }}
+              type="number"
+              name="amount"
+              placeholder="Amount"
+              value={this.state.amount}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button
+            style={{
+              marginTop: "10px",
+              borderRadius: "10px",
+              backgroundColor: "#f35f25",
+              color: "white",
+              width: "120px",
+              height: "40px",
+            }}
+            onClick={this.sendToken}
+          >
+            Send
+          </button>
+        </div>
       </div>
     );
   }
